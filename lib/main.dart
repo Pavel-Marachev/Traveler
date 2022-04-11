6397a0c3e1b6d34ac7487e28beaa3fd2c11a2b69
Pavel Marachev <pavel_marachev@senla.eu>
2022-04-11 18:05:35 +0300

    Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:traveler/business_logic/blocs/navigation_bloc/navigation_bloc.dart';
 import 'package:traveler/presentation/pages/add_route_page.dart';
+import 'package:traveler/presentation/pages/authorization_page.dart';
 
 import 'package:traveler/presentation/pages/home_page.dart';
 import 'package:traveler/presentation/pages/places_list_page.dart';
@@ -11,7 +12,10 @@ import 'package:traveler/presentation/theme/constants.dart';
 void main() async {
   runApp(BlocProvider(
       create: (context) => NavigationBloc()..add(AppStarted()),
-      child: const MyApp()));
+      child: const MaterialApp(
+        home: AuthorizationPage(),
+        debugShowCheckedModeBanner: false,
+      )));
 }
 
 class MyApp extends StatelessWidget {
@@ -41,71 +45,71 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<NavigationBloc, NavigationState>(
         builder: (context, state) {
-          return MaterialApp(
-            title: 'Traveler',
-            debugShowCheckedModeBanner: false,
-            home: Scaffold(
-              backgroundColor: kBackgroundWidgetColor,
-              bottomNavigationBar: BottomNavigationBar(
-                unselectedItemColor: kWidgetColor.withOpacity(0.6),
-                selectedItemColor: kWidgetColor,
-                backgroundColor: kMainColor.withOpacity(0.3),
-                currentIndex: getCurrentIndex(state),
-                onTap: (value) {
-                  switch (value) {
-                    case 0:
-                      context
-                          .read<NavigationBloc>()
-                          .add(PressedOnPlacesListPage());
-                      break;
-                    case 1:
-                      context.read<NavigationBloc>().add(PressedOnHomePage());
-                      break;
-                    case 2:
-                      context.read<NavigationBloc>().add(
-                          PressedOnAddRoutePage());
-                      break;
-                  }
-                },
-                items: [
-                  BottomNavigationBarItem(
-                    icon: SvgPicture.asset(
-                      "assets/images/list_icon.svg",
-                      color: kWidgetColor
-                          .withOpacity(getCurrentIndex(state) == 0 ? 1 : 0.6),
-                    ),
-                    label: "Маршруты",
-                  ),
-                  BottomNavigationBarItem(
-                    icon: SvgPicture.asset(
-                      "assets/images/home_icon.svg",
-                      color: kWidgetColor
-                          .withOpacity(getCurrentIndex(state) == 1 ? 1 : 0.6),
-                    ),
-                    label: "Главная",
-                  ),
-                  BottomNavigationBarItem(
-                    icon: SvgPicture.asset(
-                      "assets/images/travel_add_icon.svg",
-                      color: kWidgetColor
-                          .withOpacity(getCurrentIndex(state) == 2 ? 1 : 0.6),
-                    ),
-                    label: "Планирование",
-                  ),
+      return MaterialApp(
+        title: 'Traveler',
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: kBackgroundWidgetColor,
+          bottomNavigationBar: BottomNavigationBar(
+            unselectedItemColor: kWidgetColor.withOpacity(0.6),
+            selectedItemColor: kWidgetColor,
+            backgroundColor: kMainColor.withOpacity(0.3),
+            currentIndex: getCurrentIndex(state),
+            onTap: (value) {
+              switch (value) {
+                case 0:
+                  context.read<NavigationBloc>().add(PressedOnPlacesListPage());
+                  break;
+                case 1:
+                  context.read<NavigationBloc>().add(PressedOnHomePage());
+                  break;
+                case 2:
+                  context.read<NavigationBloc>().add(PressedOnAddRoutePage());
+                  break;
+              }
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  "assets/images/list_icon.svg",
+                  color: kWidgetColor
+                      .withOpacity(getCurrentIndex(state) == 0 ? 1 : 0.6),
+                ),
+                label: "Маршруты",
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  "assets/images/home_icon.svg",
+                  color: kWidgetColor
+                      .withOpacity(getCurrentIndex(state) == 1 ? 1 : 0.6),
+                ),
+                label: "Главная",
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  "assets/images/travel_add_icon.svg",
+                  color: kWidgetColor
+                      .withOpacity(getCurrentIndex(state) == 2 ? 1 : 0.6),
+                ),
+                label: "Планирование",
+              ),
+            ],
+          ),
+          body: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment(0, 1),
+                end: Alignment(1, 0.2),
+                colors: <Color>[
+                  kMainColor,
+                  kBackgroundWidgetColor.withOpacity(0.9),
                 ],
               ),
-              body: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(begin: Alignment(0, 1),
-                    end: Alignment(1, 0.2),
-                    colors: <Color> [
-                      kMainColor,
-                      kBackgroundWidgetColor.withOpacity(0.9),
-                    ],),
-                ),
-                child: getPageState(state),),
             ),
-          );
-        });
+            child: getPageState(state),
+          ),
+        ),
+      );
+    });
   }
 }
